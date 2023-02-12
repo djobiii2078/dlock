8c60f3ad470790d7e5aa026f2b7cd22fc6283e94
MVONDO DJOB BARBE THYSTERE <djobiii2078@gmail.com>
2023-02-12 15:59:50 +0100

    lock init benchmark

--- lock.c
+++ lock.c
@@ -1,17 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<pthread.h>
+
 
 #define NBPAGES 1024
+#define NBTHREADS 100
+
+pthread_mutex_t lock_processed_ops; 
 
 struct page{
 	int array[512];
-};
+}page;
 
+pthread_t thread_ids[NBTHREADS];
+int processed_ops; 
 
 int process(void *p)
 {
-	page *myPage = malloc(1024*8*sizeof(array[512]));
+	struct page *myPage = malloc(1024*8*sizeof(array[512]));
 	//initialize the array 
+	int nbfinal_ops = (int)p; 
 	int counter = 0;
 	int j = 0;
 
@@ -25,6 +33,20 @@ int process(void *p)
 	
 	//update the global counter
 	//with a lock here 
+	for(counter = 0; counter < NBPAGES; counter++)
+	{
+		for(j=0; j<512; j++)
+		{			
+			srand(time(NULL));  
+			myPage[counter].array[j]=rand()*j + rand()*j;
+			pthread_mutex_lock(&lock_processed_ops);
+			processed_ops +=1; 
+			pthread_mutex_unlock(&lock_processed_ops);  
+			printf("ops = %d/%d", processed_ops, nbfinal_ops); 
+		}
+
+	}
+
 
 }
 
@@ -32,11 +54,41 @@ int process(void *p)
 int main(void)
 {
 	//Global variable : overall_counter
+	int err; 
 	int gc_ops=0;
+	int nbthreads_counter=0;
+
 
 	//le but est de créer plusieurs threads 
 	//chaque threads réalise un traitement 
 	//lise et mette à jour gc_ops en bout de course 
-	//avant de continuer leur traitement	
+	//avant de continuer leur traitement
+
+	if (pthread_mutex_init(&lock_processed_ops, NULL) != 0)
+    {
+        printf("\n mutex initialization failed\n");
+        return 1;
+    }
+
+
+	while (nbthreads_counter<NBTHREADS)
+	{
+		err = pthread_create(&(thread_ids[nbthreads_counter]),NULL,&process,NULL);
+		if(!err)
+			printf("\ncannott create thread :[%s]", strerror(err));
+		
+		nbthreads_counter++;
+	}
+
+	while(nbthreads_counter>1)
+	{
+		pthread_join(threads_ids[nbthreads_counter--],NULL); 
+	}
+
+	pthread_mutex_destroy(&lock);
+
+	return 0; 
+
+
 }
 
